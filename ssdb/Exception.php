90e9e19930b3e8bb7b5771cda25aed31477470dc
iwu <iwu@jackwu.local>
2015-06-02 14:31:11 +0800

    update

--- ssdb/Exception.php
+++ ssdb/Exception.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Exception - yii2-ssdb
+ *
+ * @author Jack.wu <xiaowu365@gmail.com>
+ * @create_time 2015-06-02 14:29
+ */
+
 namespace ijackwu\ssdb;
 
 class Exception extends \Exception
